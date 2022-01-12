f118d7cd3f7be845d65c58b53c6adaff803a40a2
nadibogor <yellistratova48@gmail.com>
2022-01-12 20:13:28 +0700

    first commit

---
+++ app/Http/Controllers/HistoryController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\barang;
+use App\Models\pesanan;
+use App\Models\pesanan_detail;
+use App\Models\User;
+use Auth;
+use Alert;
+use Illuminate\Http\Request;
+
+class HistoryController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pesanans=pesanan::where('user_id',Auth::user()->id)->where('status','!=',0)->get();
+        return view('history.index',compact('pesanans'));
+    }
+
+    public function detail($id)
+    {
+        $pesanan=pesanan::where('id',$id)->first();
+        $pesanan_details=pesanan_detail::where('pesanan_id',$pesanan->id)->get();
+
+        return view('history.detail',compact('pesanan','pesanan_details'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 m-2">
+            <img src="{{url('images/WebsiteLogo.png')}}" class="mx-auto d-block" width="200" alt="">
+        </div>
+        @foreach($barangs as $barang)
+        <div class="col-md-4">
+            <div class="card">
+                <img src="{{url('uploads')}}/{{$barang->gambar}}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{$barang->nama_barang}}</h5>
+                    <p class="card-text">
+                        <strong>Harga :</strong> Rp.{{number_format($barang->harga)}}<br>
+                        <strong>Unit Tersisa :</strong>{{$barang->persediaan}}<br>
+                        <hr>
+                        <strong>Keterangan :</strong>{{$barang->keterangan}}
+                    </p>
+                    <a href="{{url('pesan')}}/{{$barang->id}}" class="btn btn-primary"><i class="fas fa-shopping-cart"></i> Pesan</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
